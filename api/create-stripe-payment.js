11d8902c8bfb8b1c6ada58b3a60cdff3c628e3e3
jwang1127-pixel <jwang1127@@gmail.com>
2025-10-01 19:08:12 -0700

    Fix typo in require statement

--- api/create-stripe-payment.js
+++ api/create-stripe-payment.js
@@ -1,6 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
